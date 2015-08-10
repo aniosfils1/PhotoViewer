30bc2eb200a0bda7cb3bb9e8dea0081bca08c911
Sydnee S. Charles <sydnee12sarah@yahoo.fr>
2015-08-10 14:16:11 -0700

    Login_d

--- app/src/main/java/com/example/photoviewer/photoviewer/LoginActivity.java
+++ app/src/main/java/com/example/photoviewer/photoviewer/LoginActivity.java
@@ -1,38 +1,13 @@
 package com.example.photoviewer.photoviewer;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.ContentResolver;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A login screen that offers login via email/password.
@@ -46,11 +21,11 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        AuthIG();
+
     }
 
     public void AuthIG() {
-        String url = "https://api.instagram.com/oauth/authorize/?client_id=3a738930c86844a8b505115a3c2427c0&redirect_uri=http://codepath.com&response_type=token";
+        String url = "https://api.instagram.com/oauth/authorize/?client_id=29ee23ba2c8441fa821455660f3c94a3&redirect_uri=http://codepath.com&response_type=token";
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.setVerticalScrollBarEnabled(false);
         webView.setHorizontalScrollBarEnabled(false);
@@ -64,7 +39,7 @@ public class LoginActivity extends Activity {
     }
 
     public void goToSu (View view) {
-
+        AuthIG();
     }
 
 
